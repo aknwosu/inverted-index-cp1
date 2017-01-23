d7152aa0c7b26a2387f212cfe521c776f45ef394
AK Nwosu <AK@AKs-MacBook-Pro.local>
2017-01-23 19:21:15 +0100

    created gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const run = require('gulp-run');
+const browserify = require('gulp-browserify');
+const rename = require('gulp-rename');
+
+const appSync = browserSync.create();
+const testSync = browserSync.create();
+
+
+gulp.task('scripts', () => {
+  gulp.src('jasmine/spec/inverted-index-test.js')
+    .pipe(browserify())
+    .pipe(rename('bundle.js'))
+    .pipe(gulp.dest('jasmine/spec'));
+});
+
+
+gulp.task('watch', () => {
+  gulp.watch(
+    [
+      'controllers/**/*.js',
+      'src/**/*.js',
+      'src/**/*.css',
+      'src/**/*.html',
+      'gulpfile.js'
+    ],
+    appSync.reload);
+  gulp.watch(['jasmine/**/*', 'src/**/*.js'], testSync.reload);
+
+  gulp.watch(
+    [
+      'src/inverted-index.js',
+      'jasmine/spec/inverted-index-test.js'
+    ],
+    ['scripts']);
+});
+
+gulp.task('serveApp', () => {
+  appSync.init({
+    server: {
+      baseDir: './',
+      index: 'src/index.html'
+    },
+    port: process.env.PORT || 8000,
+    ui: false,
+    ghostMode: false
+  });
+});
+
+gulp.task('serveTest', () => {
+  testSync.init({
+    server: {
+      baseDir: ['./src', './jasmine'],
+      index: 'SpecRunner.html'
+    },
+    port: 8080,
+    ui: false,
+    ghostMode: false
+  });
+});
+
+gulp.task('test', () => {
+  run('node_modules/karma/bin/karma start karma.conf.js --single-run').exec();
+});
+
+gulp.task('default', ['serveApp', 'scripts', 'watch', 'serveTest']);
